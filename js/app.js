5dd962dd598232bb903707eac3f4343927bf6f84
Paulo Duarte <phduarte87@outlook.com>
2021-10-14 23:53:09 -0300

    ajustes imagens e ícones

--- js/app.js
+++ js/app.js
@@ -22,16 +22,16 @@ ajax.onreadystatechange = function(){
 
             for(var i = 0; i <data_json.length; i++){
 
-                html_content += '<div class="row title_cat"><div class="col-12"><h2>'+data_json[i].cargo+'</h2></div></div><div class="row">';
+                html_content += '<div class="row title_cat"><div class="col-12"><h2>'+data_json[i].estilo+'</h2></div></div><div class="row">';
 
-                if(data_json[i].pessoa.length == 0){
+                if(data_json[i].musicas.length == 0){
                     html_content += '<div class="col-12"><div class="alert alert-warning" role="alert">Nenhuma música cadastrada.</div></div>';
                 }else{
                     
-                    for(var j = 0; j < data_json[i].pessoa.length; j++){
-                        html_content += card_pessoa(data_json[i].pessoa[j].nome,data_json[i].pessoa[j].imagem,data_json[i].pessoa[j].posicao,data_json[i].pessoa[j].url_linkedin,data_json[i].pessoa[j].experiencia,data_json[i].pessoa[j].formacao,data_json[i].pessoa[j].habilidades);
+                    for(var j = 0; j < data_json[i].musicas.length; j++){
+                        var musica = data_json[i].musicas[j];
+                        html_content += card_musica(musica.artista,musica.foto,musica.titulo,musica.video,musica.letra);
                     }
-
                 }
 
                 html_content +="</div>";
@@ -42,42 +42,34 @@ ajax.onreadystatechange = function(){
     }
 }
 
-var card_pessoa = function(nome, imagem, posicao, url_linkedin, experiencia, formacao, habilidades){
-
-
+var card_musica = function(nome, foto, titulo, video, letra){
     return '<div class="col-md-4">'+       
                 '<div class="card">'+
-                    '<img src="'+imagem+'" class="img_profile">'+
+                    '<img src="'+foto+'" class="img_profile">'+
                     '<h5 class="card-header">'+nome+'</h5>'+
                     '<div class="card-body" style="text-align: center;">'+
-                        '<h5 class="card-title">'+posicao+'</h5>'+
-                        '<p class="card-text">Largo conhecimento técnico e liderança.</p>'+
-                        '<a href="#" onClick="javascript:dadosModal(\''+nome+'\', \''+experiencia+'\', \''+formacao+'\', \''+habilidades+'\');" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#fullProfile">Sobre</a>&nbsp;'+
-                        '<a href="'+url_linkedin+'" target="_blank" class="btn btn-info">Linked-in</a>'+
+                        '<h5 class="card-title">'+titulo+'</h5>'+
+                        '<p class="card-text">&nbsp;</p>'+
+                        '<a href="#" onClick="javascript:dadosModal(\''+nome+'\', \''+letra+'\');" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#fullProfile">Letra</a>&nbsp;'+
+                        '<a href="'+video+'" target="_blank" class="btn btn-info">Ouça</a>'+
                     '</div>'+
                 '</div>'+
             '</div>';
 
 }
 
-var dadosModal = function(nome, experiencia, formacao, habilidades){
-
+var dadosModal = function(nome, letra){
     console.log(nome);
-
     document.getElementById('fullProfile_Title').innerHTML = nome;
-    document.getElementById('fullProfile_Experiencia').innerHTML = experiencia;
-    document.getElementById('fullProfile_Formacao').innerHTML = formacao;
-    document.getElementById('fullProfile_Habilidade').innerHTML = habilidades;
-
+    document.getElementById('fullProfile_Experiencia').innerHTML = letra;
 }
 
-//Cache Dinâmico
-
+// Cache Dinâmico
 var cache_dinamico = function(data_json){
 
     if('caches' in window){
 
-        caches.delete('connect-app-dinamico').then(function(){
+        caches.delete('kiss-app-static-files').then(function(){
 
             if(data_json.length > 0){
 
@@ -88,29 +80,22 @@ var cache_dinamico = function(data_json){
 
                         if(files.indexOf(data_json[i].pessoa[j].imagem) == -1){
                             files.push(data_json[i].pessoa[j].imagem);
-                        }
-                    
+                        }                    
                     }
                 }
-
             }
 
-            caches.open('connect-app-dinamico').then(function (cache){
+            caches.open('kiss-app-static-files').then(function (cache){
 
                 cache.addAll(files).then(function(){
-
                     console.log("Cache dinâmico realizado com sucesso!");
-
                 });
-
             });
-
         });
-
     }
 }
 
-//Interceptar o Prompt de Instalação do PWA
+// Interceptar o Prompt de Instalação do PWA
 
 let disparoInstalacao = null;
 const btInstall = document.getElementById('btInstall');
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,29 @@
 'use strict';
 
-const CACHE_NAME = "connect-app-estatico";
+const CACHE_NAME = "kiss-app-static-files";
 
 const FILES_STATIC = [
 
     'css/bootstrap.min.css',
     'css/styles.css',
+    
     'icons/favicon.ico',
     'icons/152.png',
-    'imgs/bg1.jpg',
+
+    'imgs/alicecooper.jpg',
+    'imgs/bg-footer.png',
     'imgs/cat_icon.png',
+    'imgs/engenheiros-do-hawaii.jpg',
+    'imgs/icon.png',
+    'imgs/joan-jett.jpg',
     'imgs/loading.gif',
     'imgs/logo1.png',
-    'imgs/logo2.png',
+    "imgs/megadeth.jpg",
     'imgs/offline.png',
+    
     'js/app.js',
     'js/bootstrap.bundle.min.js',
+
     'offline.html'
 ];
 
@@ -28,55 +36,41 @@ self.addEventListener('install', (evt) => {
 
             console.log("Service Worker realizando caches estáticos");
             return cache.addAll(FILES_STATIC);
-
         })
-
     );
-    self.skipWaiting();
 
+    self.skipWaiting();
 });
 
 //Ativação
 self.addEventListener('activate', (evt) => {
 
     evt.waitUntil(
-
             caches.keys().then((keylist) => {
-
                 return Promise.all(keylist.map((key) => {
 
                     if(key !== CACHE_NAME){
                         return caches.delete(key);
                     }
-
                 }));
-
             })
-
     );
 
     self.clients.claim();
-
 });
 
 //Responder um página Offline (fetch)
 
 self.addEventListener('fetch', (evt) => {
-
     if(evt.request.mode !== 'navigate'){
         return;
     }
 
     evt.respondWith(
-
         fetch(evt.request).catch(() => {
             return caches.open(CACHE_NAME).then((cache) => {
-
                 return cache.match('offline.html');
-
             })
         })
-
     );
-
 });
